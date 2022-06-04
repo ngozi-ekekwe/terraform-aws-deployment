f1aa02b455ad1fbbdda72b75f3d550a99d6404b8
Ngozi Ekekwe <ngozi-ekekwe>
2022-06-04 21:07:25 +0200

    feat: add terraform config for aws ecr

---
+++ terraform/registry/variables.tf
@@ -0,0 +1,15 @@
+variable "provider_region" {
+  description = "Value of the Provider "
+  type        = string
+}
+
+variable "provider_profile" {
+  description = "The profile for the provider"
+  type        = string
+}
+
+
+variable "registry_name" {
+  description = "Value for the registry"
+  type        = string
+}
