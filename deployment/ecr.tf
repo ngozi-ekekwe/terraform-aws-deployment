84d1aa6ea1bcc616d9b890614d746a76e57ae38e
Ngozi Ekekwe <ngozi-ekekwe>
2022-06-06 15:07:45 +0200

    feat: add github actions

--- deployment/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "repository" {
-  name                 = var.registry_name
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- deployment/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region  = var.provider_region
-  profile = var.provider_profile
-}
